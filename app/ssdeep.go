2e2057449d5b1e52f1f6015998b8975eaacf3f51
Bernhard Reisenberger <b.reisenberger@nimbusec.com>
2017-03-07 11:42:52 +0100

    updated example

--- app/ssdeep.go
+++ app/ssdeep.go
@@ -3,8 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
-	"github.com/glaslos/ssdeep"
+	"github.com/bernhardreisenberger/ssdeep"
 )
 
 func main() {
@@ -15,19 +16,42 @@ func main() {
 	}
 	if len(flag.Args()) == 1 {
 		sdeep := ssdeep.NewSSDEEP()
-		fmt.Println(sdeep.Fuzzy(flag.Args()[0]))
-	}
-	if len(flag.Args()) == 2 {
-		sdeep := ssdeep.NewSSDEEP()
-		h1 := sdeep.Fuzzy(flag.Args()[0])
-		sdeep = ssdeep.NewSSDEEP()
-		h2 := sdeep.Fuzzy(flag.Args()[1])
-		fmt.Printf("%s\n%s\n", h1, h2)
-		score, err := ssdeep.HashDistance(h1, h2)
+		file, err := os.Open(flag.Args()[0])
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		hash, err := sdeep.Fuzzy(file)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%v", &hash)
+	}
+	if len(flag.Args()) == 2 {
+		sdeep := ssdeep.NewSSDEEP()
+		f1, err := os.Open(flag.Args()[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		h1, err := sdeep.Fuzzy(f1)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		sdeep = ssdeep.NewSSDEEP()
+		f2, err := os.Open(flag.Args()[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		h2, err := sdeep.Fuzzy(f2)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		score := ssdeep.HashDistance(*h1, *h2)
 		fmt.Println(score)
 	}
 }
